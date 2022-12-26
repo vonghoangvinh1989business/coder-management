918777eb4a821387380cf44d87f9cae6c3d61d27
Vong Hoang Vinh <vonghoangvinh1989@gmail.com>
2022-12-26 21:35:10 +0700

    add code template for coder-management

---
+++ app.js
@@ -0,0 +1,56 @@
+const { sendResponse, AppError } = require("./helpers/utils.js");
+require("dotenv").config();
+const cors = require("cors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+
+var indexRouter = require("./routes/index");
+
+var app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(cors());
+
+// require mongoose library
+const mongoose = require("mongoose");
+
+/* DB Connection */
+const mongoURI = process.env.MONGODB_URI;
+
+// suppress the warning
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(mongoURI)
+  .then(() => console.log(`DB connected ${mongoURI}`))
+  .catch((error) => console.log(error));
+
+app.use("/", indexRouter);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new AppError(404, "Not Found", "Bad Request");
+  next(err);
+});
+
+/* Initialize Error Handling */
+app.use((err, req, res, next) => {
+  console.log("ERROR", err);
+  return sendResponse(
+    res,
+    err.statusCode ? err.statusCode : 500,
+    false,
+    null,
+    { message: err.message },
+    err.isOperational ? err.errorType : "Internal Server Error"
+  );
+});
+
+module.exports = app;
